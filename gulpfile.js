b3c442776de186bb60bf6380a7929c7a6096cbd5
Ankur Nair <ankur.nair@cesltd.com>
2017-03-14 14:55:31 +0530

    init

--- gulpfile.js
+++ gulpfile.js
@@ -143,4 +143,12 @@ gulp.task('build', ['less'], function(done) {
   done();
 });
 
+gulp.task('serveprod', function() {
+  connect.server({
+    root: './www',
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
+
 gulp.task('default', ['server']);
